5a29479d92ea0bad732bc63b5b1a7647ac9e3008
diegooliveiralima <diegooliveiralima@gmail.com>
2021-03-07 00:45:19 -0300

    criacao

--- nao_entre_em_panico.py
+++ nao_entre_em_panico.py
@@ -734,7 +734,7 @@ for each in range(3):
 #---------------------------------------------- Disparo com o IFFT - TELEGRAM -------------------   
 @app.route("/ifttt", methods=["GET"])
 def handler(): 
-    EntryContent = request.get_data(as_text=True)
+    
     datetime.datetime.now()
     datetime.datetime(2009, 1, 6, 15, 8, 24, 78915)
     print("Hora do disparo:")
