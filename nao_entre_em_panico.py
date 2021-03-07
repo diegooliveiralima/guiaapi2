57971ee95b1261346015f48a32cd826b43dc944d
diegooliveiralima <diegooliveiralima@gmail.com>
2021-03-07 16:38:43 -0300

    criacao

--- nao_entre_em_panico.py
+++ nao_entre_em_panico.py
@@ -710,8 +710,7 @@ def nao_entre_em_panico():
    
    
     
-    if request.headers.get('Authorization') == '42':
-        return jsonify({"42": "a resposta para a vida, o universo e tudo mais"})
+    
     return jsonify({"message": "Não entre em pânico!"})
 
 
