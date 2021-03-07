c6041848e944c023a760e8311dc27977814a0376
diegooliveiralima <diegooliveiralima@gmail.com>
2021-03-07 00:59:23 -0300

    criacao

--- nao_entre_em_panico.py
+++ nao_entre_em_panico.py
@@ -24,6 +24,136 @@ import io
 import ebooklib
 
 
+
+
+'''
+url = "https://www.google.com.br/alerts/feeds/06114678530526052316/7725213029379595095"
+
+resp = requests.get(url)
+
+soup = BeautifulSoup(resp.content, features="xml")
+
+items = soup.findAll('entry')
+
+item = items[0]
+
+print(item.title.text)
+print(item.link.get('href'))
+'''
+
+
+'''
+response = {"value1":"jantar", "value2":"08/15 17:00", "value3":"08/15 17:00"}
+requests.post("https://hooks.zapier.com/hooks/catch/8251891/ofm655u", data=response)
+'''
+
+'''
+
+url = "http://www.unilab.edu.br/restauranteuniversitario/"
+req = requests.get(url)
+soup = BeautifulSoup(req.content, 'html.parser')
+
+links = soup.find_all('a', href=re.compile(r'(\.pdf)'))
+links = links[0]['href']
+links = str(links)
+links = re.sub('Ç', r'%C3%87', links)
+
+myurl = links
+
+with urllib.request.urlopen(myurl) as url:
+    s = url.read()
+    
+with open(r"c:\tmp\test.pdf", "wb") as f:
+    f.write(s)
+
+pdf_path = r"c:\tmp\test.pdf"
+
+
+tables = camelot.read_pdf(pdf_path)
+
+
+tables.export('foo.csv', f='csv', compress=True) # json, excel, html, sqlite
+tabela = tables[0]
+
+tables[0].parsing_report
+{
+    'accuracy': 99.02,
+    'whitespace': 12.24,
+    'order': 1,
+    'page': 1
+}
+
+#print(tables[0].df) # get a pandas DataFrame!
+
+tables[0].to_json('foo.json')
+
+with open('foo.json') as f:
+    l = json.load(f)
+    print(l)
+diaColuna = date.today().weekday() + 1  #Dia da semana + 1 é a formula
+diaColuna = str(diaColuna)
+coluna = diaColuna 
+if coluna == "7":
+    pass
+else:
+
+
+    princpal1   =  l[2][coluna]
+    princpal2   =  l[3][coluna]   
+    vegetariano =  l[4][coluna]  
+    salada1     =  l[5][coluna]  
+    salada2     =  l[6][coluna]  
+    guarnicao   =  l[7][coluna] 
+    acompanha1  =  l[8][coluna]
+    acompanha2  =  l[9][coluna] 
+    acompanha3  =  l[10][coluna]
+    suco        =  l[11][coluna]
+    sobremesa1  =  l[12][coluna]
+    sobremesa2  =  l[13][coluna]
+
+    nprincpal1   =  l[15][coluna]
+    nprincpal2   =  l[16][coluna]   
+    nvegetariano =  l[17][coluna]  
+    nsalada1     =  l[18][coluna]  
+    nsalada2     =  l[19][coluna]  
+    nguarnicao   =  l[20][coluna] 
+    nacompanha1  =  l[21][coluna]
+    nacompanha2  =  l[22][coluna] 
+    nacompanha3  =  l[23][coluna]
+    sopa         =  l[24][coluna]
+    acompSopa    =  l[25][coluna]
+    nsuco        =  l[26][coluna]
+    nsobremesa1  =  l[27][coluna]
+    nsobremesa2  =  l[28][coluna]
+
+    inicio  =  l[0]["1"]
+    termino  = l[0]["6"]
+
+
+    cardapioManha = "<b>Principal:</b> " + princpal1 + ", "  + princpal2 + "\n" + "Vegetariano: " + vegetariano + "\n" + "Saladas: " + salada1 + ", " + salada2 +  "\n" + "Guarnição: " + guarnicao  + "\n" + "Acompanhamentos: " + acompanha1 + ", " + acompanha2 + ", " + acompanha3 + "\n" + "Suco: " + suco + "\n" + "Sobremesas: " + sobremesa1 + ", " + sobremesa2
+    print(cardapioManha)
+
+    cardapioNoite =  "<b>Principal:</b> " + nprincpal1 + ", "  + nprincpal2 + "\n" + "Vegetariano: " + nvegetariano + "\n" + "Saladas: " + nsalada1 + ", " + nsalada2 +  "\n" + "Guarnição: " + nguarnicao  + "\n" + "Acompanhamentos: " + nacompanha1 + ", " + nacompanha2 + ", " + nacompanha3 + "\n" + "Sopa: " + sopa + "\n" + "Acompanhamento da sopa: " + acompSopa + "\n" +  "Suco: " + nsuco + "\n" + "Sobremesas: " + nsobremesa1 + ", " + nsobremesa2
+
+    TOKEN = "1335874302:AAGfJS-I6j8QJL1vpU_oryvlX0_4ZvnJSms"
+    bot = telegram.Bot(TOKEN)
+    print("Bot do telegram conectado!")
+    chat_id = "608446615"
+               
+    txt = "CARDÁPIO - SEMANA - " + inicio + " até " + termino + "\n" + "\n" + "Manhã: " + "\n" + cardapioManha + "\n" + "\n" +  "Noite: " + "\n" + cardapioNoite
+    bot.send_message(chat_id, txt, parse_mode='html')   
+'''
+
+
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=["POST"])
+def nao_entre_em_panico():
+    return jsonify({"message": "Não entre em pânico!"})
+
+
 users = ("deathproof","austinburke", "silentdawn" )
     
 g = 0
@@ -194,136 +324,6 @@ try:
 
 except:
     print("error")
-
-
-'''
-url = "https://www.google.com.br/alerts/feeds/06114678530526052316/7725213029379595095"
-
-resp = requests.get(url)
-
-soup = BeautifulSoup(resp.content, features="xml")
-
-items = soup.findAll('entry')
-
-item = items[0]
-
-print(item.title.text)
-print(item.link.get('href'))
-'''
-
-
-'''
-response = {"value1":"jantar", "value2":"08/15 17:00", "value3":"08/15 17:00"}
-requests.post("https://hooks.zapier.com/hooks/catch/8251891/ofm655u", data=response)
-'''
-
-'''
-
-url = "http://www.unilab.edu.br/restauranteuniversitario/"
-req = requests.get(url)
-soup = BeautifulSoup(req.content, 'html.parser')
-
-links = soup.find_all('a', href=re.compile(r'(\.pdf)'))
-links = links[0]['href']
-links = str(links)
-links = re.sub('Ç', r'%C3%87', links)
-
-myurl = links
-
-with urllib.request.urlopen(myurl) as url:
-    s = url.read()
-    
-with open(r"c:\tmp\test.pdf", "wb") as f:
-    f.write(s)
-
-pdf_path = r"c:\tmp\test.pdf"
-
-
-tables = camelot.read_pdf(pdf_path)
-
-
-tables.export('foo.csv', f='csv', compress=True) # json, excel, html, sqlite
-tabela = tables[0]
-
-tables[0].parsing_report
-{
-    'accuracy': 99.02,
-    'whitespace': 12.24,
-    'order': 1,
-    'page': 1
-}
-
-#print(tables[0].df) # get a pandas DataFrame!
-
-tables[0].to_json('foo.json')
-
-with open('foo.json') as f:
-    l = json.load(f)
-    print(l)
-diaColuna = date.today().weekday() + 1  #Dia da semana + 1 é a formula
-diaColuna = str(diaColuna)
-coluna = diaColuna 
-if coluna == "7":
-    pass
-else:
-
-
-    princpal1   =  l[2][coluna]
-    princpal2   =  l[3][coluna]   
-    vegetariano =  l[4][coluna]  
-    salada1     =  l[5][coluna]  
-    salada2     =  l[6][coluna]  
-    guarnicao   =  l[7][coluna] 
-    acompanha1  =  l[8][coluna]
-    acompanha2  =  l[9][coluna] 
-    acompanha3  =  l[10][coluna]
-    suco        =  l[11][coluna]
-    sobremesa1  =  l[12][coluna]
-    sobremesa2  =  l[13][coluna]
-
-    nprincpal1   =  l[15][coluna]
-    nprincpal2   =  l[16][coluna]   
-    nvegetariano =  l[17][coluna]  
-    nsalada1     =  l[18][coluna]  
-    nsalada2     =  l[19][coluna]  
-    nguarnicao   =  l[20][coluna] 
-    nacompanha1  =  l[21][coluna]
-    nacompanha2  =  l[22][coluna] 
-    nacompanha3  =  l[23][coluna]
-    sopa         =  l[24][coluna]
-    acompSopa    =  l[25][coluna]
-    nsuco        =  l[26][coluna]
-    nsobremesa1  =  l[27][coluna]
-    nsobremesa2  =  l[28][coluna]
-
-    inicio  =  l[0]["1"]
-    termino  = l[0]["6"]
-
-
-    cardapioManha = "<b>Principal:</b> " + princpal1 + ", "  + princpal2 + "\n" + "Vegetariano: " + vegetariano + "\n" + "Saladas: " + salada1 + ", " + salada2 +  "\n" + "Guarnição: " + guarnicao  + "\n" + "Acompanhamentos: " + acompanha1 + ", " + acompanha2 + ", " + acompanha3 + "\n" + "Suco: " + suco + "\n" + "Sobremesas: " + sobremesa1 + ", " + sobremesa2
-    print(cardapioManha)
-
-    cardapioNoite =  "<b>Principal:</b> " + nprincpal1 + ", "  + nprincpal2 + "\n" + "Vegetariano: " + nvegetariano + "\n" + "Saladas: " + nsalada1 + ", " + nsalada2 +  "\n" + "Guarnição: " + nguarnicao  + "\n" + "Acompanhamentos: " + nacompanha1 + ", " + nacompanha2 + ", " + nacompanha3 + "\n" + "Sopa: " + sopa + "\n" + "Acompanhamento da sopa: " + acompSopa + "\n" +  "Suco: " + nsuco + "\n" + "Sobremesas: " + nsobremesa1 + ", " + nsobremesa2
-
-    TOKEN = "1335874302:AAGfJS-I6j8QJL1vpU_oryvlX0_4ZvnJSms"
-    bot = telegram.Bot(TOKEN)
-    print("Bot do telegram conectado!")
-    chat_id = "608446615"
-               
-    txt = "CARDÁPIO - SEMANA - " + inicio + " até " + termino + "\n" + "\n" + "Manhã: " + "\n" + cardapioManha + "\n" + "\n" +  "Noite: " + "\n" + cardapioNoite
-    bot.send_message(chat_id, txt, parse_mode='html')   
-'''
-
-
-
-
-app = Flask(__name__)
-
-@app.route('/')
-def nao_entre_em_panico():
-    return jsonify({"message": "Não entre em pânico!"})
-
-
             
         
    
